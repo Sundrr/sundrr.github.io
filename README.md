4bb3f2510da758269bb3019b337639d150e6b4f0
kuzmin.e <Kuzmin.e@o-code.ru>
2022-11-25 11:45:27 +0400

    upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# sundrr.github.io
+# table-app
+
+Страница с таблицей, сверстанная по макету Figma.
+
+Макет: https://www.figma.com/file/1ovzpJ8JY2SnmiWsvsSvqC/Dashboard-Advanced?node-id=0%3A1&t=Rt5ga1chzQneAp8U-0
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <table-page />
+</template>
+
+<script>
+import TablePage from '@/components/pages/TablePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TablePage,
+  }
+}
+</script>
+
+<style>
+  div {
+    display: initial;
+  }
+
+ p {
+   margin: 0;
+ }
+</style>
